a564a3fa292fb5f8288f30b1644761f956bf16eb
Wei Weng <wengwei01@gmail.com>
2026-04-10 00:58:34 -0400

    Create common test util

--- tests/opencl_test_utils.h
+++ tests/opencl_test_utils.h
@@ -1,42 +1,15 @@
 #pragma once
 
-#include <fstream>
-#include <iostream>
-#include <sstream>
 #include <stdexcept>
-#include <string>
 #include <vector>
 
 #include <gtest/gtest.h>
 
+#include "test_utils.h"
+
 #include "../inc/OpenCL/cl.hpp"
 
-#define SIB_ANSI_RESET "\x1b[0m"
-#define SIB_ANSI_GREEN "\x1b[32m"
-
-#define SIB_TEST_LOG_COLOR(color, label, msg_stream)                           \
-  do {                                                                         \
-    std::ostringstream sib_test_log_oss;                                       \
-    sib_test_log_oss << msg_stream;                                            \
-    std::cerr << (color) << "[ " << (label) << " ]"                            \
-              << ((color)[0] != '\0' ? SIB_ANSI_RESET : "") << " "             \
-              << sib_test_log_oss.str() << "\n"                                \
-              << std::flush;                                                   \
-  } while (0)
-
-#define SIB_TEST_LOG(label, msg_stream)                                        \
-  SIB_TEST_LOG_COLOR("", label, msg_stream)
-
 namespace SiberneticTest {
-
-inline std::string readTextFile(const std::string &path) {
-  std::ifstream in(path);
-  if (!in) {
-    throw std::runtime_error("Failed to open file: " + path);
-  }
-  return std::string((std::istreambuf_iterator<char>(in)),
-                     std::istreambuf_iterator<char>());
-}
 
 inline cl::Device pickDevice() {
   std::vector<cl::Platform> platforms;
---
+++ tests/test_utils.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#define SIB_ANSI_RESET "\x1b[0m"
+#define SIB_ANSI_GREEN "\x1b[32m"
+
+#define SIB_TEST_LOG_COLOR(color, label, msg_stream)                           \
+  do {                                                                         \
+    std::ostringstream sib_test_log_oss;                                       \
+    sib_test_log_oss << msg_stream;                                            \
+    std::cerr << (color) << "[ " << (label) << " ]"                            \
+              << ((color)[0] != '\0' ? SIB_ANSI_RESET : "") << " "             \
+              << sib_test_log_oss.str() << "\n"                                \
+              << std::flush;                                                   \
+  } while (0)
+
+#define SIB_TEST_LOG(label, msg_stream)                                        \
+  SIB_TEST_LOG_COLOR("", label, msg_stream)
+
+namespace SiberneticTest {
+
+inline std::string readTextFile(const std::string &path) {
+  std::ifstream in(path);
+  if (!in) {
+    throw std::runtime_error("Failed to open file: " + path);
+  }
+  return std::string((std::istreambuf_iterator<char>(in)),
+                     std::istreambuf_iterator<char>());
+}
+
+} // namespace SiberneticTest
